f5f00c6502497d2f1ca46228c42ef5263cc7215d
NabilaValia <nabilaputrivaliandra29@gmail.com>
2024-09-24 16:01:28 +0700

    ModifikasiPemilihanBilangan11.java

---
+++ ModifikasiPemilihanBilangan11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ModifikasiPemilihanBilangan11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+
+        String jenisBilangan = (angka % 2 == 0) ? "genap" : "ganjil";
+        System.out.print("Angka " + angka + " termasuk bilangan " + jenisBilangan);
+
+   
+}
+}
